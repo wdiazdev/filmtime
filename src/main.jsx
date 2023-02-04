b92c88cd8ae0df30f41859ad76a48d90af0f9093
Wilfredo Diaz <wdiazdev@gmail.com>
2023-02-04 15:22:05 -0500

    Scripts

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import { HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App'
 import { AuthContextProvider } from './Context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <HashRouter> */}
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-    {/* </HashRouter> */}
+    <HashRouter>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </HashRouter>
   </React.StrictMode>,
 );
