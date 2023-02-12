ef6a2db9680f0ebb0bd2964782303748ad79c1d8
Wilfredo Diaz <wdiazdev@gmail.com>
2023-02-11 22:10:22 -0500

    MyList

--- src/Pages/MyList.jsx
+++ src/Pages/MyList.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
-
+import '../Styles/MyList.css';
+import myListBg from '../assets/playerbg.jpg';
 export default function MyList() {
     return (
-        <div>MyList</div>
+        <div className='my--list--container'>
+
+            <img src={myListBg} alt="" />
+
+            <div className='overlay'></div>
+
+            <h2>Your Saved Movies</h2>
+
+            <div className='my--list--wrapper'>
+
+            </div>
+        </div>
     )
 }
